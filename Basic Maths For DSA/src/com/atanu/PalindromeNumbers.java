d1eace779abd331be354da7ccc9697814a4eefdb
Atanu <Atanu.Chakrabarti@Chubb.com>
2024-01-10 21:42:58 +0530

    added palindrome number

---
+++ Basic Maths For DSA/src/com/atanu/PalindromeNumbers.java
@@ -0,0 +1,20 @@
+package com.atanu;
+
+public class PalindromeNumbers {
+
+    public static void main(String[] args) {
+        int n = 84569;
+        System.out.println(palindrome(n));
+    }
+
+    // TC : O(d), where d is the number of digits in n
+    // SC : O(1)
+    private static boolean palindrome(int n) {
+        int rev = 0, temp = n;
+        while (n != 0) {
+            rev = rev*10 + n%10;
+            n = n/10;
+        }
+        return (temp == rev);
+    }
+}
