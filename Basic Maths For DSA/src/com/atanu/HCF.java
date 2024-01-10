41c3a9e01bd41668f3b5590b8061e66368271ede
Atanu <Atanu.Chakrabarti@Chubb.com>
2024-01-10 23:04:38 +0530

    added hcf and lcm

---
+++ Basic Maths For DSA/src/com/atanu/HCF.java
@@ -0,0 +1,42 @@
+package com.atanu;
+
+public class HCF {
+
+    public static void main(String[] args) {
+        int a = 12, b = 15;
+        System.out.println(hcf1(a, b));
+        System.out.println(hcf2(a, b));
+        System.out.println(hcf3(a, b));
+    }
+
+    // TC : O(min(a,b))
+    // SC : O(1)
+    private static int hcf1(int a, int b) {
+        int min = Math.min(a, b);
+        while (min != 0) {
+            if (a % min == 0 && b % min == 0)
+                break;
+            min--;
+        }
+        return min;
+    }
+
+    // TC : O(max(a,b))
+    // SC : O(1)
+    private static int hcf2(int a, int b) {
+        while (a != b) {
+            if (a > b)
+                a = a-b;
+            else
+                b = b-a;
+        }
+        return a;
+    }
+
+    // TC : O(log(min(a,b)))
+    // SC : O(1)
+    private static int hcf3(int a, int b) {
+        if (b == 0) return a;
+        return hcf3(b, a%b);
+    }
+}
---
+++ Basic Maths For DSA/src/com/atanu/LCM.java
@@ -0,0 +1,35 @@
+package com.atanu;
+
+public class LCM {
+
+    public static void main(String[] args) {
+        int a = 12, b = 15;
+        System.out.println(lcm1(a, b));
+        System.out.println(lcm2(a, b));
+    }
+
+    // TC : O(a*b-max(a,b))
+    // SC : O(1)
+    private static int lcm1(int a, int b) {
+        int max = Math.max(a, b);
+        while (true) {
+            if (max % a == 0 && max % b == 0)
+                break;
+            max++;
+        }
+        return max;
+    }
+
+    // TC : O(log(min(a,b)))
+    // SC : O(1)
+    private static int lcm2(int a, int b) {
+        return (a*b)/gcf(a,b);
+    }
+
+    // TC : O(log(min(a,b)))
+    // SC : O(1)
+    private static int gcf(int a, int b) {
+        if (b == 0) return a;
+        return gcf(b, a%b);
+    }
+}
