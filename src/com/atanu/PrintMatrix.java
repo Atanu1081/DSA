6ff40fef39800b257050d4cb4bc6f3eead4eb4b0
Atanu <atanu1996@gmail.com>
2024-01-25 13:18:54 +0530

    my first commit

---
+++ src/com/atanu/PrintMatrix.java
@@ -0,0 +1,22 @@
+package com.atanu;
+
+/**
+ * @author Atanu
+ */
+public class PrintMatrix {
+    public static void main(String[] args) {
+        int[ ][ ] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+        print(matrix);
+    }
+
+    private static void print(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+}
